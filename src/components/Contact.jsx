1a9eadc68c6a5c5593156b98ee4256f11772bc00
Hugo A.Morais <hugones45@gmail.com>
2023-06-20 08:33:18 -0300

    modificacoes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,6 @@ export const Contact = () => {
             <form method='POST' action="https://getform.io/f/9653134b-cd26-4d97-a922-e20be4112cdd" className='flex flex-col max-w-[600px] w-full'>
 
                 <div className='pb-8'>
-
                     <p className='text-4xl font-bold inline border-b-4 text-gray-300 dark:text-[#000] border-pink-600'>Contact</p>
                     <p className='text-gray-300 dark:text-[#000] py-4'>Submit the Form Below or shoot me an email - hugones45@gmail.com</p>
                 </div>
